342dac5a7674c97663230c163c9620992a9babfd
Quang Hung <quanghungleo@gmail.com>
2018-10-23 10:22:08 +0700

    Post

---
+++ WindowStoreApplication/Views/PhotoGallery.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace WindowStoreApplication.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class PhotoGallery : Page
+    {
+        public PhotoGallery()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void getPhotoClick(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                this.textBlock.Text = "";
+                this.fileName.Text = file.Name;
+                this.filePath.Text = file.Path;
+                this.fileDate.Text = file.DateCreated.ToString();
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                var image = new BitmapImage();
+                image.SetSource(stream);
+                thumbnail.Source = image;
+            }
+            else
+            {
+                this.textBlock.Text = "Operation cancelled.";
+                this.fileName.Text = "";
+                this.filePath.Text = "";
+                this.fileDate.Text = "";
+            }
+        }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.Frame.CanGoBack) {
+                this.Frame.GoBack();
+            }
+        }
+    }
+}
